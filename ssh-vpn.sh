2f3f522314c802fb83c90ae6090c8398221d45bb
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-08-16 15:16:00 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -234,7 +234,7 @@ netfilter-persistent reload
 
 # download script
 cd /usr/bin
-wget -O add-host "https://raw.githubusercontent.com/Nataslamet/Natascript/main/add-host.sh"
+wget -O add-host "https://raw.githubusercontent.com/fardinzaga/scriptssh/master/menu/add-host.sh"
 wget -O about "https://raw.githubusercontent.com/Nataslamet/Natascript/main/about.sh"
 wget -O menu "https://raw.githubusercontent.com/Nataslamet/Natascript/main/menu.sh"
 wget -O usernew "https://raw.githubusercontent.com/Nataslamet/Natascript/main/usernew.sh"
