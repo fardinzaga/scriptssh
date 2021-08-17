1916d5a6dae6a16894aefaf87682f36824165669
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-08-17 17:53:25 +0700

    Update set-br.sh

--- set-br.sh
+++ set-br.sh
@@ -26,12 +26,12 @@ logfile ~/.msmtp.log
 EOF
 chown -R www-data:www-data /etc/msmtprc
 cd /usr/bin
-wget -O autobackup "https://raw.githubusercontent.com/Nataslamet/Natascript/main/autobackup.sh"
-wget -O backup "https://raw.githubusercontent.com/Nataslamet/Natascript/main/backup.sh"
-wget -O bckp "https://raw.githubusercontent.com/Nataslamet/Natascript/main/bckp.sh"
-wget -O restore "https://raw.githubusercontent.com/Nataslamet/Natascript/main/restore.sh"
+wget -O autobackup "https://raw.githubusercontent.com/fardinzaga/scriptssh/master/br/autobackup.sh"
+wget -O backup "https://raw.githubusercontent.com/fardinzaga/scriptssh/master/br/backup.sh"
+wget -O bckp "https://raw.githubusercontent.com/fardinzaga/scriptssh/master/br/bckp.sh"
+wget -O restore "https://raw.githubusercontent.com/fardinzaga/scriptssh/master/br/restore.sh"
 wget -O strt "https://raw.githubusercontent.com/Nataslamet/Natascript/main/strt.sh"
-wget -O limit-speed "https://raw.githubusercontent.com/Nataslamet/Natascript/main/limit-speed.sh"
+wget -O limit-speed "https://raw.githubusercontent.com/fardinzaga/scriptssh/master/br/limit-speed.sh"
 chmod +x autobackup
 chmod +x backup
 chmod +x bckp
