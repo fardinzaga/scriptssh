9ac8fcfe151679e9b9743ab048b9aa011fae7623
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-08-20 22:43:00 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -90,7 +90,7 @@ rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
 wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/fardinzaga/scriptssh/master/nginx/nginx.conf"
 mkdir -p /home/vps/public_html
-wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/Nataslamet/Natascript/main/vps.conf"
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/fardinzaga/scriptssh/master/nginx/vps.conf"
 /etc/init.d/nginx restart
 
 # install badvpn
